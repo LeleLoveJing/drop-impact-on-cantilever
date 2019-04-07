e3ecf3f0e97dd941d09e5e6f1f6ea10d5a05d7ba
Michael Negus <michaelnegus1@gmail.com>
2019-04-08 00:34:00 +0100

    Added definitions for cantilever position
    Wrote analytic solution for s(t), s'(t) and d(t). Also added a gitignore to ignore asv files.

--- MATLAB code/mass_only.m
+++ MATLAB code/mass_only.m
@@ -2,8 +2,11 @@
 % 
 % This script deals with the distinguished limit where the mass term of the
 % ODE for the cantilever dominates over the damping and spring term. In
-% this case we have an analytic solution for s_0(t) for all time.
+% this case we have an analytic solution for s_0(t) for all time. We drop
+% the 0 subscripts.
 
 %%
 % Solution for s_0(t) and other quantities
-
+s = @(t) 0.5 + t - 0.5 * sqrt(1 + 4 * t);
+sdot = @(t) 1 - 1 / sqrt(1 + 4 * t);
+d = @(t) 2 * sqrt(t - s(t));
