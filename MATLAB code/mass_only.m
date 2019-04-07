e6552ec13fbe5f497d6049a3dff128d0a5965318
Michael Negus <michaelnegus1@gmail.com>
2019-04-08 00:12:28 +0100

    Create mass_only.m
    Script for the case where the mass term in the ODE for the cantilever dominates over damping and spring terms

---
+++ MATLAB code/mass_only.m
@@ -0,0 +1,9 @@
+%% Droplet impact on a cantilever: mass_only.m
+% 
+% This script deals with the distinguished limit where the mass term of the
+% ODE for the cantilever dominates over the damping and spring term. In
+% this case we have an analytic solution for s_0(t) for all time.
+
+%%
+% Solution for s_0(t) and other quantities
+
