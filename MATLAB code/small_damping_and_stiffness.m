b77301921f44d90ddeb99ce84846d4338ccf76b8
Michael Negus <michaelnegus1@gmail.com>
2019-04-16 16:48:51 +0100

    Update small_damping_and_stiffness.m
    Changed file to be a function that outputs the desired arrays

--- MATLAB code/small_damping_and_stiffness.m
+++ MATLAB code/small_damping_and_stiffness.m
@@ -1,59 +1,43 @@
-%% Droplet impact on a cantilever: small_damping_and_stiffness.m
-% 
-% This script deals with the distinguised limit where there is a small
-% amount of damping, characterised by beta and a small amount of stiffness,
-% characterised by delta, which is larger than the square root of the mass 
-% ratio alpha but still much smaller than 1. It plots the first order 
-% correction to the cantilever displacement s_0(t) to the case where there 
-% is no damping or stiffness. 
+function [s, sdot, sddot] = asymptotic_solution(tvals, beta, delta)
+%ASYMPTOTIC_SOLUTION Gives the asymptotic solution to the cantilever
+%displacement for small damping beta and stiffness delta.
+%   This function deals with the distinguised limit where there is a small
+%   amount of damping, characterised by beta, and a small amount of
+%   stiffness, characterised by delta, which are both larger than the
+%   square root of the mass ratio, characterised by sqrt(alpha). The
+%   solutions were derived using matched asymptotic expansions, so are in
+%   the form of first order corrections to a homogeneous solution,
+%   proportional to beta and delta. The input is tvals, an array for times
+%   which want to be plotted, and then the physical parameters beta and
+%   delta. It outputs an array for the cantilever displacement s and its
+%   first and second derivatives sdot and sddot. 
 
-%%
-% Solution for cantilever displacement and its derivatives
-beta = 1e-2;
-delta = 1e-3;
+% Functional homogeneous solution with no damping or stiffness
+s0fun = @(t) 0.5 + t - 0.5 * sqrt(1 + 4 * t); 
+sdot0fun = @(t) 1 - 1 ./ sqrt(1 + 4 * t);
+sddot0fun = @(t) 2 * (1 + 4 * t).^(-3/2);
 
-% Homogeneous solution with no damping or stiffness
-s0 = @(t) 0.5 + t - 0.5 * sqrt(1 + 4 * t); 
-sdot0 = @(t) 1 - 1 ./ sqrt(1 + 4 * t);
-sddot0 = @(t) 2 * (1 + 4 * t).^(-3/2);
-
-s = @(t) s0(t) ...
+% Functional solution for solution with first order corrections for damping
+% and stiffness
+sfun = @(t) s0fun(t) ...
     - (beta / 12) * ((1 + 6 * t + 6 * t.^2) ./ sqrt(1 + 4 * t) - 1 - 4 * t) ...
     - (delta / 120) ...
         * ((1 + 10 * t + 30 * t.^2 + 20 * t.^3) ./ sqrt(1 + 4 * t) ...
             - (1 + 4 * t).^2);
-sdot = @(t) sdot0(t) ...
+sdotfun = @(t) sdot0fun(t) ...
     - (beta / 3) * ((1 + 6 * t + 9 * t.^2) / (1 + 4 * t).^(3/2) - 1) ...
     - (delta / 15) ...
         * ((1 + 10 * t + 30 * t.^2 + 25 * t.^3) ./ (1 + 4 * t).^(3/2) ...
             - (1 + 4 * t));
-sddot = @(t) sddot0(t) ...
+sddotfun = @(t) sddot0fun(t) ...
     - 2 * beta * (1 + 3 * t) ./ (1 + 4 * t).^(5/2) ...
     - (delta / 15) ...
         * ((4 + 40 * t + 135 * t.^2 + 150 * t.^3) ./ (1 + 4 * t).^(5/2) - 4);
+
+% Solutions for s, sdot and sddot as arrays over the given time
+s = sfun(tvals);
+sdot = sdotfun(tvals);
+sddot = sddotfun(tvals);
+
+end
  
-%%
-% Later time solution for t > (12 / delta)^(2/3)
-slate = @(t) t - (12/delta)^(2/3) * sqrt(5 * (delta / 12)^(2/3) * t - 4);
-
-scomp = @(t) s0(t) + slate(t) - t;
-%%
-% Messing
-tdelta = (12 / delta)^(2/3)
-
-
-%%
-% Creating plots for a specific time array using the function
-% plotting_cantilever
-tvals1 = 0.01:0.01:1.1 * tdelta;
-tvals2 = 0.9 * tdelta:0.01:3 * tdelta;
-tvalstot = 0.01:0.01:3 * tdelta;
-
-hold on
-plot(tvalstot, s(tvalstot));
-plot(tvalstot, slate(tvalstot));
-plot(tvalstot, scomp(tvalstot));
-hold off
-legend("s1", "s2", "scomp");
-
-% plotting_cantilever(tvals, s(tvals), sdot(tvals), sddot(tvals));
