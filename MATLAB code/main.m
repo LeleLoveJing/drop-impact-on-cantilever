aa6542fb8aeb10c1ab7e9b728082e20a06e09368
Michael Negus <michaelnegus1@gmail.com>
2019-04-24 22:55:27 +0100

    Update main.m
    Added section to plot the energies

--- MATLAB code/main.m
+++ MATLAB code/main.m
@@ -8,25 +8,62 @@
 % accurate for small delta and beta, by plotting the results against those
 % found numerically. 
 
-beta = 1e-3; % Damping coefficient
-delta = 1e-3; % Stiffness coefficient
+% beta = 1e-3; % Damping coefficient
+% delta = 1e-3; % Stiffness coefficient
+% 
+% tvals = 0.01:0.001:100; % Time range
+% 
+% % Asymptotic solution
+% [s_asy, sdot_asy, sddot_asy] = asymptotic_solution(tvals, beta, delta);
+% 
+% % Numerical solution
+% [s_num, sdot_num, sddot_num] = numerical_solution(tvals, beta, delta);
+% 
+% % Homogeneous solution (no damping or stiffness)
+% [s_homo, sdot_homo, sddot_homo] = asymptotic_solution(tvals, 0, 0);
+% 
+% % Plotting s
+% figure(1);
+% hold on;
+% plot(tvals, s_asy);
+% plot(tvals, s_num);
+% plot(tvals, s_homo);
+% hold off;
+% legend("Asymptotic", "Numerical", "Homogeneous");
 
-tvals = 0.01:0.001:100; % Time range
+%% Energy calculations
+% This section is to calculate the energies of a specific problem, plot
+% them, and see if they are as we expect. 
 
-% Asymptotic solution
-[s_asy, sdot_asy, sddot_asy] = asymptotic_solution(tvals, beta, delta);
+% Physical parameters
+epsilon = 1e-4;
+beta = 1;
+delta = 1;
 
-% Numerical solution
-[s_num, sdot_num, sddot_num] = numerical_solution(tvals, beta, delta);
+tvals = 0.01:0.01:4; % Time values
 
-% Homogeneous solution (no damping or stiffness)
-[s_homo, sdot_homo, sddot_homo] = asymptotic_solution(tvals, 0, 0);
+% Solves the problem numerically
+[s, sdot, sddot] = numerical_solution(tvals, beta, delta);
+figure(1)
+plot(tvals, s)
 
-% Plotting s
-figure(1);
-hold on;
-plot(tvals, s_asy);
-plot(tvals, s_num);
-plot(tvals, s_homo);
-hold off;
-legend("Asymptotic", "Numerical", "Homogeneous");
+tvals = tvals'; % Transposes for ease of addition
+
+% Calculates the energies
+[outer_outer_energy, outer_energy, jet_energy, work_done, energy_diss] ...
+    = energies(tvals, s, sdot, sddot, epsilon, beta);
+
+drop_energy = outer_outer_energy + outer_energy + jet_energy;
+cantilever_energy = work_done - energy_diss;
+total_energy = drop_energy + cantilever_energy;
+
+figure(2);
+hold on
+plot(tvals, outer_outer_energy - 1);
+plot(tvals, outer_energy);
+plot(tvals, jet_energy);
+plot(tvals, work_done);
+plot(tvals, -energy_diss);
+plot(tvals, total_energy  - 1);
+hold off
+legend("Outer-outer - 1", "Outer", "Jet", "Work done", "Energy dissipated", "Total - 1");
