26d9b3bab82f6f69f797f101268474b827cb8ce4
MNegus <michaelnegus1@gmail.com>
2019-05-16 11:38:33 +0100

    Included improved asymptotic solution

--- MATLAB code/large_delta.m
+++ MATLAB code/large_delta.m
@@ -1,27 +1,43 @@
 % Parameters
-beta = 0;
+beta = 1;
 delta = 1e4;
 
-tmax = 1000 / sqrt(delta) ; % Maximum value of time
-tstep = tmax / 1000; % Time steps
+tmax = 100; % Maximum value of time
+tstep = tmax / 100000; % Time steps
 tvals = 0 : tstep : tmax; % Time domain
 
 % Asymptotic expansions for large delta, beta << 1
 s_inner = tvals.^2;
-s_middle = (2 / delta) * (1 - cos(sqrt(delta) * tvals));
+s_middle_altered = (2 / delta) * (1 - cos(sqrt(delta) * tvals));
+% s_middle = (2 / delta) * (1 - cos(sqrt(delta) * tvals));
+
 
 % Numerical calculation
 [s, sdot, sddot] = numerical_solution(tvals, beta, delta);
 
+B = - sqrt(delta) - atan((3 + beta) / (2 * sqrt(delta)));
+A = -2 / cos(B + sqrt(delta));
 
+
+sol = 2/delta + (A/delta) * cos(sqrt(delta) * sqrt(1 + 2 * tvals) + B) ...
+    .* (1 + 2 * tvals).^(-(3 + beta) / 4);
+
+B = -sqrt(delta);
+A = -2;
+simple_sol = 2/delta + (A/delta) * cos(sqrt(delta) * sqrt(1 + 2 * tvals) + B) ...
+    .* (1 + 2 * tvals).^(-(3 + beta) / 4);
 % Creating plots
 close all
 
+s_asy = (2/delta) * (1 - cos(sqrt(delta) * (sqrt(1 + 2 * tvals) - 1)) ...
+.* (1 + 2 * tvals).^(-(3 + beta) / 4)); 
+
 figure(1);
 hold on
-plot(tvals, s);
-% plot(tvals, s_middle);
-hold off
-xlabel("t");
-ylabel("s(t)");
+plot(tvals, s_inner);
+plot(tvals, sol);
 legend("Numerical", "Asymptotic");
+
+figure(2);
+plot(tvals, sdot);
+
